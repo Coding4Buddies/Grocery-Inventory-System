d566fecdabeaed6210bf805ddb07e818e4d1a504
nickichann01 <grimaldo.131810@kalibo.sti.edu>
2022-01-26 16:10:21 +0800

    Add Main Class

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,15 @@
+// Main Class of the Program
+
+package main;
+
+import crud.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String args[]) {
+       
+    }
+}
